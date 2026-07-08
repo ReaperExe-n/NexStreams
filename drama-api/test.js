6665abcb157717186d706ce91ea42a41d5767cc6
NexStreams <nexstreams@user.com>
2026-07-08 23:48:29 +0545

    fix: remove tsc from build script to fix vercel deployment

--- drama-api/test.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-axios.get('https://dramacool9.com.ro/vincenzo-2021-episode-20.html').then(res => {
-    const $ = cheerio.load(res.data);
-    require('fs').writeFileSync('iframes.txt', $('iframe').map((i, el) => $(el).attr('src')).get().join('\n'));
-});
